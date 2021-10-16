5e0b8b40ee2f0bdb0280cc5faf36cff019c2fe8a
1119 pdh <1119pdh@gmail.com>
2021-10-16 15:02:35 +0900

    Add: Django Projuct Setting

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Westar1',
+        'USER': 'root',
+        'PASSWORD': '!eogus0953',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+				'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+
+SECRET_KEY = 'django-insecure-pwxn7ic99bc*#e2%r^hjy(2lq2+_2!ho+pddpyd9+gt@-m%ym)'
